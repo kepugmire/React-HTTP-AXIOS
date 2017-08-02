fcaf8f449d3c2d2f10a28661d6cd1c1ea075ebbd
Kellen Pugmire <kepugmire@gmail.com>
2017-08-02 11:00:08 -0600

    Initial commit to repo

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+
+
+    }
+  }
+
+  componentDidMount(){
+    axios.get('http://pokeapi.co/api/v2/pokemon/1')
+        .then(function(response){
+          console.log(response)
+        })
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
